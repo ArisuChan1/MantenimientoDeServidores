8163faafd493ef129701cde9874ecc2b7fcf132a
ArisuChan1 <107023625+ArisuChan1@users.noreply.github.com>
2024-05-26 20:15:55 -0600

    Ultimos Cambios solicitados

--- App/src/app/modules/reporte-mantenimientos/components/main/main.component.ts
+++ App/src/app/modules/reporte-mantenimientos/components/main/main.component.ts
@@ -68,6 +68,17 @@ export class MainComponent {
                     this.getEstadoMantenimientoById(mantenimiento.idEstado)
                 );
             });
+        this.mantenimientosByYear = this.mantenimientosByYear.filter(
+            (mantenimiento) => {
+                const baseDeDatos = this.getBaseDeDatosById(
+                    mantenimiento.idBaseDeDatos
+                );
+                if (baseDeDatos) {
+                    return baseDeDatos.aplicaMantenimiento;
+                }
+                return true;
+            }
+        );
         this.setMantenimientosByServidor();
         this.setMantenimientosByBaseDeDatos();
     }
@@ -80,15 +91,15 @@ export class MainComponent {
 
     getBasesDeDatos() {
         this.generalService.BASE_DE_DATOS.get().subscribe((res) => {
-            this.basesDeDatos = res;
-            this.setMantenimientosByBaseDeDatos();
+            this.basesDeDatos = res.filter((db) => db.aplicaMantenimiento);
+            this.filterMantenimientos();
         });
     }
 
     getServidores() {
         this.generalService.SERVIDORES.get().subscribe((res) => {
             this.servidores = res;
-            this.setMantenimientosByServidor();
+            this.filterMantenimientos();
         });
     }
 
