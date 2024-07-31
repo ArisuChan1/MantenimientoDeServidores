ffc1bc1fdc13d3f8944d6636f78b596470cbfd46
crywhat7 <floresalicia2002@gmail.com>
2024-07-30 22:11:37 -0600

    Graficos y reportes en el dashboard

--- App/src/app/modules/dashboard/components/main/main.component.ts
+++ App/src/app/modules/dashboard/components/main/main.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BaseDeDatos, Mantenimiento, Servidor } from 'src/app/interfaces/types';
 import { routesConfig } from 'src/app/routes/routesConfig';
 import { GeneralService } from 'src/app/services/general.service';
+import Chart from 'chart.js/auto';
 
 @Component({
     selector: 'app-main',
@@ -16,6 +17,9 @@ export class MainComponent {
     mantenimientosWPerfilCompleted: Mantenimiento[] = [];
     mantenimientosDBAutomaticosMesActual: Mantenimiento[] = [];
     mantenimientosDbAutomaticosRunning: Mantenimiento[] = [];
+
+    barChart: Chart<'bar', any[], any>;
+    pastelChart;
     constructor(private generalService: GeneralService) {
         this.getBasesDeDatos();
         this.getServidores();
@@ -41,6 +45,8 @@ export class MainComponent {
             this.setMantenimientosWPerfilCompleted();
             this.setMantenimientosDBAutomaticosMesActual();
             this.setMantenimientosDbAutomaticosRunning();
+            this.setBarChart();
+            this.setPastelChart();
         });
     }
 
@@ -77,6 +83,114 @@ export class MainComponent {
                 (mantenimiento) =>
                     mantenimiento.idEstado === 2 || mantenimiento.idEstado === 3
             );
+    }
+
+    setBarChart() {
+        // Todos los mantenimientos en cada mes del año
+        this.barChart = new Chart('cvxBarChart', {
+            type: 'bar',
+            options: {
+                plugins: {
+                    title: {
+                        text: 'Mantenimientos a lo largo del año',
+                        display: true,
+                    },
+                },
+            },
+            data: {
+                labels: this.getMonths(),
+                datasets: [
+                    {
+                        label: 'Mantenimientos',
+                        data: this.getMantenimientosByMonth(),
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 1,
+                    },
+                ],
+            },
+        });
+    }
+
+    setPastelChart() {
+        // Mantenimientos a bases de datos vs mantenimientos a servidores
+        // altura 300px
+        this.pastelChart = new Chart('cvxPastelChart', {
+            type: 'doughnut',
+            options: {
+                plugins: {
+                    title: {
+                        text: 'Mantenimientos a bases de datos vs servidores',
+                        display: true,
+                    },
+                },
+            },
+            data: {
+                labels: ['Bases de datos', 'Servidores'],
+                datasets: [
+                    {
+                        data: [
+                            this.mantenimientos.filter(
+                                (mantenimiento) =>
+                                    mantenimiento.idBaseDeDatos !== null
+                            ).length,
+                            this.mantenimientos.filter(
+                                (mantenimiento) =>
+                                    mantenimiento.idServidor !== null
+                            ).length,
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                        ],
+                        borderWidth: 1,
+                    },
+                ],
+            },
+        });
+    }
+
+    getMonths() {
+        const monthsAsString = [
+            'Enero',
+            'Febrero',
+            'Marzo',
+            'Abril',
+            'Mayo',
+            'Junio',
+            'Julio',
+            'Agosto',
+            'Septiembre',
+            'Octubre',
+            'Noviembre',
+            'Diciembre',
+        ];
+        const months = [];
+        for (let i = 0; i < 12; i++) {
+            months.push(monthsAsString[i]);
+        }
+        return months;
+    }
+
+    getMantenimientosByMonth() {
+        const mantenimientosByMonth = [];
+        for (let i = 0; i < 12; i++) {
+            const month = i + 1;
+            const mantenimientos = this.mantenimientos.filter(
+                (mantenimiento) =>
+                    new Date(mantenimiento.fechaInicio).getMonth() === i
+            );
+            mantenimientosByMonth.push(mantenimientos.length);
+        }
+        return mantenimientosByMonth;
+    }
+
+    getMonthFormatted(month: number) {
+        return this.getNumberFormatted(month + 1);
     }
 
     getNumberFormatted(value: number) {
